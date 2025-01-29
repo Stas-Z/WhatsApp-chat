aaad0b46457530c9eff61f24f979c6ff191c789f
Stas-Z <stanislav@zettafoods.ru>
2025-01-29 19:36:40 +0200

    add eslint

--- config/build/buildViteConfig.ts
+++ config/build/buildViteConfig.ts
@@ -1,30 +1,34 @@
-import { UserConfig } from 'vite';
+import autoprefixer from 'autoprefixer'
+import { UserConfig } from 'vite'
 
-import { buildOptions } from './buildOptions';
-import { buildPlugins } from './buildPlugins';
-import { buildResolvers } from './buildResolvers';
-import { buildServer } from './buildServer';
-import { BuildOptions } from './types/config';
+import { buildOptions } from './buildOptions'
+import { buildPlugins } from './buildPlugins'
+import { buildResolvers } from './buildResolvers'
+import { buildServer } from './buildServer'
+import { BuildOptions } from './types/config'
 
 export function buildViteConfig(options: BuildOptions): UserConfig {
-	const { mode, isDev } = options;
-	return {
-		mode,
-		base: mode === 'production' ? '/frontend-challenge/' : '/',
-		plugins: buildPlugins(),
-		resolve: buildResolvers(options),
-		build: buildOptions(options),
-		define: {
-			__IS_DEV__: JSON.stringify(true),
-			__API__: JSON.stringify(options.apiUrl),
-		},
-		css: {
-			modules: {
-				generateScopedName: isDev
-					? '[path][name]__[local]--[hash:base64:5]'
-					: '[hash:base64:8]',
-			},
-		},
-		server: buildServer(options),
-	};
+    const { mode, isDev } = options
+    return {
+        mode,
+        base: mode === 'production' ? '/frontend-challenge/' : '/',
+        plugins: buildPlugins(),
+        resolve: buildResolvers(options),
+        build: buildOptions(options),
+        define: {
+            __IS_DEV__: JSON.stringify(isDev),
+            __API__: JSON.stringify(options.apiUrl),
+        },
+        css: {
+            modules: {
+                generateScopedName: isDev
+                    ? '[path][name]__[local]--[hash:base64:5]'
+                    : '[hash:base64:8]',
+            },
+            postcss: {
+                plugins: [autoprefixer()],
+            },
+        },
+        server: buildServer(options),
+    }
 }
---
+++ eslint.config.js
@@ -0,0 +1,90 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import pluginImport from 'eslint-plugin-import';
+import pluginUnusedImports from 'eslint-plugin-unused-imports';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{ files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+	{ languageOptions: { globals: globals.browser } },
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+	pluginReact.configs.flat.recommended,
+
+	{
+		plugins: {
+			'react-hooks': pluginReactHooks,
+			'unused-imports': pluginUnusedImports,
+			import: pluginImport,
+		},
+		rules: {
+			'import/no-cycle': [2, { maxDepth: 1 }],
+			'react/jsx-max-props-per-line': ['error', { maximum: 4 }],
+			'react/jsx-filename-extension': [
+				2,
+				{
+					extensions: ['.js', '.jsx', '.tsx'],
+				},
+			],
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'error',
+				{ argsIgnorePattern: '^_' },
+			],
+			'unused-imports/no-unused-imports': 'error',
+			'unused-imports/no-unused-vars': [
+				'warn',
+				{
+					vars: 'all',
+					varsIgnorePattern: '^_',
+					args: 'after-used',
+					argsIgnorePattern: '^_',
+				},
+			],
+			'@typescript-eslint/naming-convention': [
+				'error',
+				{
+					selector: ['variable'],
+					modifiers: ['const'],
+					format: [
+						'strictCamelCase',
+						'StrictPascalCase',
+						'UPPER_CASE',
+					],
+					leadingUnderscore: 'allow',
+				},
+			],
+			semi: ['error', 'never'],
+
+			'react-hooks/rules-of-hooks': 'error',
+			'react-hooks/exhaustive-deps': 'error',
+			'import/order': [
+				'error',
+				{
+					groups: ['builtin', 'external', 'internal'],
+					pathGroups: [
+						{
+							pattern: 'react',
+							group: 'external',
+							position: 'before',
+						},
+						{
+							pattern: '@/**',
+							group: 'external',
+							position: 'after',
+						},
+					],
+					pathGroupsExcludedImportTypes: ['react'],
+					'newlines-between': 'always',
+					alphabetize: {
+						order: 'asc',
+						caseInsensitive: true,
+					},
+				},
+			],
+		},
+	},
+];
