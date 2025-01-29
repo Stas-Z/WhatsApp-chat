0eb6c14126f714511d866fbee17151965c799589
Stas-Z <stanislav@zettafoods.ru>
2025-01-29 17:12:48 +0200

    add vite

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,21 @@
+import path from 'path';
+
+import react from '@vitejs/plugin-react-swc';
+import { visualizer } from 'rollup-plugin-visualizer';
+import { PluginOption } from 'vite';
+import checker from 'vite-plugin-checker';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export function buildPlugins(): PluginOption[] {
+	const plugins = [
+		react(),
+		tsconfigPaths({
+			projects: [path.resolve(__dirname, '..', '..', 'tsconfig.json')],
+		}),
+		visualizer(),
+		checker({
+			typescript: true,
+		}),
+	];
+	return plugins;
+}
